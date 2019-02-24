770dfdd5a4b118f9d4711bb633d7f614e342cb9c
Nikhil Kamat <nnkamat@ncsu.edu>
2019-02-24 16:35:11 -0500

    Final Commit before Submission

--- HW2/h.c
+++ HW2/h.c
@@ -32,24 +32,11 @@ typedef struct {
   size_t used;
   size_t size;
 } Heap;
-
-/* 
- * ===  FUNCTION  ======================================================================
- *         Name:  initHeap
- *  Description:  Initialises a Dynamic Array to Create a Heap
- * =====================================================================================
- */
 void initHeap(Heap *h, size_t initialSize) {
   h->inst=(Node*)malloc(initialSize*sizeof(Node));
   h->used=0;
   h->size=initialSize;
 }
-/*
- * ===  FUNCTION  ======================================================================
- *         Name:  printHeap
- *  Description:  Prints all elements of the Heap
- * =====================================================================================
- */
 void printHeap(Heap *h)
 {
     int i;
@@ -58,22 +45,10 @@ void printHeap(Heap *h)
         printf("%d %d\n",h->inst[i].key,h->inst[i].value);
     }
 }
-/*
- * ===  FUNCTION  ======================================================================
- *         Name:  parent
- *  Description:  Returns the parent of a node
- * =====================================================================================
- */
 int parent(int n)
 {
       return ((n-1)>>shft);
 }
-/* 
- * ===  FUNCTION  ======================================================================
- *         Name:  percolateUp
- *  Description:  Percolates a Node up the Heap
- * =====================================================================================
- */
 void percolateUp(Heap *h,int n)
 {
     if(parent(n)>=0)
@@ -88,12 +63,6 @@ void percolateUp(Heap *h,int n)
         }
     }
 }
-/* 
- * ===  FUNCTION  ======================================================================
- *         Name:  insertValue
- *  Description:  Inserts a Value into the Heap
- * =====================================================================================
- */
 void insertValue(Heap *h, int k, int v)
 {
     if(h->used == h->size)
@@ -107,23 +76,11 @@ void insertValue(Heap *h, int k, int v)
     percolateUp(h,h->used);
     h->used++;
 }
-/* 
- * ===  FUNCTION  ======================================================================
- *         Name:  freeHeap
- *  Description:  Release the memory allocated for the Heap
- * =====================================================================================
- */
 void freeHeap(Heap *h) {
   free(h->inst);
   h->inst=NULL;
   h->used=h->size=0;
 }
-/* 
- * ===  FUNCTION  ======================================================================
- *         Name:  MinHeapify
- *  Description:  Build an Min-Heap from an array
- * =====================================================================================
- */
 void MinHeapify(Heap *h,int i)
 {
     int smaller,j,min;
@@ -150,12 +107,6 @@ void MinHeapify(Heap *h,int i)
         }
     }
 }
-/* 
- * ===  FUNCTION  ======================================================================
- *         Name:  removeMin
- *  Description:  Removes the Minimum Value from the Heap
- * =====================================================================================
- */
 void removeMin(Heap *h,int *k,int *v)
 {
     *k=h->inst[0].key;
@@ -168,13 +119,6 @@ void removeMin(Heap *h,int *k,int *v)
 
     MinHeapify(h,0);
 }
-
-/* 
- * ===  FUNCTION  ======================================================================
- *         Name:  main
- *  Description:  For Handling Input and Function Calls
- * =====================================================================================
- */
 int main(int argc, char** argv)
 {
     int j;
@@ -183,7 +127,6 @@ int main(int argc, char** argv)
     int rkey,rvalue;
     Heap h;
     initHeap(&h,2);
-    //Valid Input
     if(argc<2)
         bf=2;
     else if(argc==2)
@@ -194,7 +137,7 @@ int main(int argc, char** argv)
         exit(1);
     }
     //Branching Factor Power of 2
-    if(((bf&(bf-1))!=0) || (bf<2) )
+    if((bf&(bf-1))!=0)
     {
         printf("Please check Branching Factor \n");
         exit(1);
@@ -208,7 +151,6 @@ int main(int argc, char** argv)
             j=j/2;
         }
     }
-    //Heap Operations
     while(scanf("%s",in)!=EOF)
     {
         if(in[0]!='-')
